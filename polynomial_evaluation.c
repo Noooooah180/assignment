b71ad444aaaa8acc4fdd7943f38ae8b58da4091a
Noooooah180 <100886079+Noooooah180@users.noreply.github.com>
2022-05-06 08:24:54 +0800

    Terrific

--- polynomial_evaluation.c
+++ polynomial_evaluation.c
@@ -1,53 +1,15 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<math.h>
 
-//return x^q
-double power(double x, int q) {
-
-    if (q == 0) {
-        return 1;
-    }
-
-    double m = x;
-    for (int i = 1; i < q; i++) {
-        //HERE
-        //this line should do some operation to achieve multiplications
-        //only ONE line to be ADD
-        //please refer to page 6 of Operation slides to get some idea
-    }
-    return m;
-}
-
-int main() {
-    //prompt user to type in x and y
-    printf("請輸入浮點數x的值以及多項式含有的項數正整數y︰");
-
-    //variable declaration
-    double x;
+int main(){
+    double x,sum;
     int y;
-
-    //get from standard input
-    scanf("%lf%d", &x, &y);
-
-    //initial result
-    double result = 0.0;
-
-    //array declaration, to keep the polynomial with size of 6
-    double p[6];
-    int q[6];
-
-    //iterate each line
-    for (int i = 0; i < y; i++) {
-        //fetch input
-        scanf("%lf%d", &p[i], &q[i]);
-
-        //value cumulation
-        //HERE
-        //this line should do some operation to achieve summations
-        //the left variable and right expression should be UNTOUCHED
-        //only the OPERATION to be MODIFIED
-        //please refer to page 6 of Operation slides to get some idea
-        result = p[i] * power(x, q[i]);
+    scanf("%lf%d",&x,&y);
+    double A[y];
+    int B[y];
+    for(int i=0 ;i<y;i++){
+        scanf("%lf%d",&A[i],&B[i]);
+        sum+=A[i]*pow(x,B[i]);
     }
-
-    printf("%lf\n", result);
+    printf("%lf",sum);
 }
